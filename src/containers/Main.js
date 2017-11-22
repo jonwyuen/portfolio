2aa26a07c01dbb4b88f4940aa79f772b6ef71ee7
Jonathan Yuen <jonwyuen@gmail.com>
2017-11-21 19:05:57 -0800

    about and projects components

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import About from "../components/About"
+import Projects from "../components/projects"
 import './Main.css';
 
 class Main extends Component {
   render() {
     return (
       <div className="Main">
-        
+        <About />
+        <Projects />
       </div>
     );
   }
