37f4e7b15648076e99e2a2810b08b5d9c2a2d98c
Jonathan Yuen <jonwyuen@gmail.com>
2017-11-23 01:08:01 -0800

    some styling on nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,19 +6,19 @@ class Navigation extends Component {
   render() {
     return (
       <div className='nav'>
-        <Navbar fixedTop={true}>
+        <Navbar fixedTop={true} bsStyle='inverse'>
           <Navbar.Header>
             <Navbar.Brand>
-              <a href='#home'>Jonathan Yuen</a>
+              <a href='#home'>J/Y</a>
             </Navbar.Brand>
             <Navbar.Toggle />
           </Navbar.Header>
           <Navbar.Collapse>
-            <Nav className='nav-right'>
-              <NavItem eventKey={1} href='#about'>About me</NavItem>
-              <NavItem eventKey={2} href='#skills'>Skills</NavItem>
-              <NavItem eventKey={3} href='#projects'>Projects</NavItem>
-              <NavItem eventKey={4} href='#contact'>Contact</NavItem>
+            <Nav pullRight={true}>
+              <NavItem eventKey={1} href='#about'>&#60; About me /></NavItem>
+              <NavItem eventKey={2} href='#skills'>&#60; Skills /></NavItem>
+              <NavItem eventKey={3} href='#projects'>&#60; Projects /></NavItem>
+              <NavItem eventKey={4} href='#contact'>&#60; Contact /></NavItem>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
