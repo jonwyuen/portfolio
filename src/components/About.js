b49da7d98c4627ab39f0dd3cbf59eea62f504247
Jonathan Yuen <jonwyuen@gmail.com>
2017-11-23 02:48:40 -0800

    working on about me

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,13 @@ import './About.css';
 
 class About extends Component {
   render() {
-    return <div className='about' />;
+    return (
+      <div className='about'>
+        <div>
+          Hi, I'm Jonathan. I have always loved to solve problems, no matter the difficulty or magnitude of the problem. What I enjoy the most is not the result, but the process of solving the problem because I believe that I am always learning something new. This belief and my passion for technology is what drives me to become a successful software developer. I hope you can become part of my journey!
+        </div>
+      </div>
+    )
   }
 }
 
