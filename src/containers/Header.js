325daea670437bc7b65bccb2f99ba07043982586
Jonathan Yuen <jonwyuen@gmail.com>
2017-11-26 02:13:20 -0800

    adding media icons to header

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -15,6 +15,11 @@ class Header extends Component {
             <h1>Jonathan Yuen</h1>
             <h3><em>A full stack web developer from the Bay Area</em></h3>
           </div>
+          <div className='media-icons'>
+            <a href="mailto:jownyuen@gmail.com"><span className='fa fa-envelope-o'></span></a>
+            <a href="https://www.linkedin.com/in/jonathanyuen"><span className='fa fa-linkedin'></span></a>
+            <a href="https://github.com/jonwyuen"><span className='fa fa-github'></span></a>
+          </div>
         </div>
       </div>
     );
