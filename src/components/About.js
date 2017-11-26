07cc0945696396e391103a7a9ac65b7285e75aae
Jonathan Yuen <jonwyuen@gmail.com>
2017-11-26 01:19:16 -0800

    fixing about me with more info

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,10 @@ class About extends Component {
       <div className='about'>
         <span><img src={aboutImage} alt='Jonathan Yuen'/></span>
         <div className='about-text'>
-          Hi, I'm Jonathan. I have always loved to solve problems, no matter the difficulty or magnitude of the problem. What I enjoy the most is not the result, but the process of solving the problem because I believe that I am always learning something new. This belief and my passion for technology is what drives me to become a successful software developer. I hope you can become part of my journey!
+          Hi, I'm Jonathan. I'm a software developer born and raised in San Francisco. Ever since I was little, I have been fascinated by how new technologies are being developed so frequently. This curiosity has inevitably led to the evaporation of my parents' money and my collection of cell phones, laptops, and various electronics.
+        </div>
+        <div className='about-text'>
+          I have always loved to solve problems, no matter the difficulty or magnitude of the problem. What I enjoy the most is not the result, but the process of solving the problem because I believe that I am always learning something. My passion for learning and technology is what drives me to become a successful software developer. I hope you can become part of my journey!
         </div>
       </div>
     )
