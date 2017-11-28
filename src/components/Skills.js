bcec270858032fff73027c80d427dae399ed4857
Jonathan Yuen <jonwyuen@gmail.com>
2017-11-28 01:15:22 -0800

    skills updated with styling

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,18 +6,31 @@ class Skills extends Component {
     return (
       <div className='skills'>
         <div className='skill-group'>
-          <h4>Front End Technologies</h4>
-          <p>
-            
-          </p>
+          <span className='fa fa-laptop'></span>
+          <h3>Front End Technologies</h3>
+          <div className="skill-detail">
+            <p>
+              Some front end technologies I have learned and worked with are <strong>JavaScript, ReactJS, Redux, jQuery, HTML5, CSS3.</strong>
+            </p>
+          </div>
         </div>
         <div className='skill-group'>
-          <h4>Back End Technologies</h4>
-          <p></p>
+          <span className='fa fa-server'></span>
+          <h3>Back End Technologies</h3>
+          <div className="skill-detail">
+            <p>
+              Some back end technologies I have learned and worked with are <strong>Node.js, Express, MongoDB, Python, Flask, PostgreSQL.</strong>
+            </p>
+          </div>
         </div>
         <div className='skill-group'>
-          <h4>Team Communication</h4>
-          <p></p>
+          <span className='fa fa-users'></span>
+          <h3>Team Communication</h3>
+          <div className="skill-detail">
+            <p>
+              Working previously for a small company, I have learned how crucial effective team communication is towards success.
+            </p>
+          </div>
         </div>
       </div>
     )
