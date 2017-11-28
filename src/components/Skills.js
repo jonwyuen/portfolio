dae4bc9f3844c9127efaa76062de5c47c375c1d3
Jonathan Yuen <jonwyuen@gmail.com>
2017-11-28 00:45:57 -0800

    working on skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,7 +5,20 @@ class Skills extends Component {
   render() {
     return (
       <div className='skills'>
-
+        <div className='skill-group'>
+          <h4>Front End Technologies</h4>
+          <p>
+            
+          </p>
+        </div>
+        <div className='skill-group'>
+          <h4>Back End Technologies</h4>
+          <p></p>
+        </div>
+        <div className='skill-group'>
+          <h4>Team Communication</h4>
+          <p></p>
+        </div>
       </div>
     )
   }
