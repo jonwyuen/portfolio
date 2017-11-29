7f4dcd80ff86f17d47df27267d8abbd412a1927b
Jonathan Yuen <jonwyuen@gmail.com>
2017-11-29 04:25:01 -0800

    working on projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,7 +3,42 @@ import './Projects.css';
 
 class Projects extends Component {
   render() {
-    return <div className='projects' />;
+    return (
+      <div className='projects'>
+        <div className='project-section'>
+          <span></span>
+          <h3>Tunelark</h3>
+          <div className="project-detail">
+            <p>
+            </p>
+          </div>
+        </div>
+        <div className='project-section'>
+          <span></span>
+          <h3>UCSF Cams</h3>
+          <div className="project-detail">
+            <p>
+            </p>
+          </div>
+        </div>
+        <div className='project-section'>
+          <span></span>
+          <h3>MapIt</h3>
+          <div className="project-detail">
+            <p>
+            </p>
+          </div>
+        </div>
+        <div className='project-section'>
+          <span></span>
+          <h3>Warbler</h3>
+          <div className="project-detail">
+            <p>
+            </p>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
