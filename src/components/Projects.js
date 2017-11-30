65aeb9d6a1cec0bdb0380cbdc9655869c002eeaa
Jonathan Yuen <jonwyuen@gmail.com>
2017-11-29 23:59:16 -0800

    working on project details

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,44 +1,50 @@
-import React, { Component } from 'react';
-import './Projects.css';
+import React, { Component } from "react";
+import "./Projects.css";
 
 class Projects extends Component {
   render() {
     return (
-      <div className='projects'>
-        <div className='project-section'>
-          <span></span>
+      <div className="projects">
+        <h2>Company Projects</h2>
+        <div className="project-section">
+          <span />
           <h3>Tunelark</h3>
           <div className="project-detail">
-            <p>
-            </p>
+            <p />
           </div>
         </div>
-        <div className='project-section'>
-          <span></span>
+        <div className="project-section">
+          <span />
           <h3>UCSF Cams</h3>
           <div className="project-detail">
-            <p>
-            </p>
+            <p />
           </div>
         </div>
-        <div className='project-section'>
-          <span></span>
+        <h2>Solo Projects</h2>
+        <div className="project-section">
+          <span />
           <h3>MapIt</h3>
           <div className="project-detail">
-            <p>
-            </p>
+            <p />
           </div>
         </div>
-        <div className='project-section'>
-          <span></span>
+        <div className="project-section">
+          <span />
           <h3>Warbler</h3>
           <div className="project-detail">
+            <div className='project-tech'>
+              Technologies Used: 
+              <p>
+                
+              </p>
+            </div>
             <p>
+              A social media platform modeled after Twitter, in which users can exchange messages.
             </p>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
